6e0b3409a65bcae5a3ef315e620e64b55919454c
Victoriaaston <Aston.Victoria27@gmail.com>
2023-03-20 11:58:20 -0400

    create login page and add the route

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import viteLogo from '/vite.svg'
 import {Route, Routes} from "react-router-dom"
 import './App.css'
 import IndexPage from './pages/IndexPage'
+import LoginPage from './pages/LoginPage'
 
 function App() {
   return (
     <Routes>
       <Route index element={<IndexPage />} />
+      <Route path ="/login" element={<LoginPage />} />
     </Routes>
   )
 }
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,5 @@
+export default function LoginPage() {
+    return(
+        "Login Form shouls go here"
+    )
+}
