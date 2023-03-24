714204014bd7519c370a37738b7b673bfb0d4d24
Victoriaaston <Aston.Victoria27@gmail.com>
2023-03-24 09:49:59 -0400

    update Header.jsx imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import {Route, Routes} from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
 import './App.css'
 import IndexPage from './pages/IndexPage'
 import LoginPage from './pages/LoginPage'
--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,4 +1,4 @@
-import Header from "../../Header"
+import Header from "./pages/Header"
 import { Outlet } from "react-router-dom"
 
 export default function Layout() {
